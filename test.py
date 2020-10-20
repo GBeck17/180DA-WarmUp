48fffa0233f71c28978903c412ce890e1d245407
Greyson <gbeck17@g.ucla.edu>
2020-10-20 06:25:00 +0100

    second commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     x = "ECE_180_DA_DB"
     if x == "EE_180_DA_DB":
-        print("You are living in 2020")
+        print("You are living in 2017")
     else: 
         # comment
         x = x + " - Best class ever"
